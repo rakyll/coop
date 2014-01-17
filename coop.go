5280a92f5cb07e25ab58866e8af1e94a293cfcb8
Burcu Dogan <burcud@google.com>
2014-01-17 21:05:06 +0100

    Initial.

---
+++ coop.go
@@ -0,0 +1,157 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// coop contains some of the most commonly used concurrent
+// flows. This package is mostly around for reference. even though
+// you can use it as a library, I'd suggest you to implement your own
+// control mechanisms.
+package coop
+
+import (
+	"sync"
+	"time"
+)
+
+// Runs fn at the specified time.
+func At(t time.Time, fn func()) (done <-chan bool) {
+	return After(t.Sub(time.Now()), fn)
+}
+
+// Runs until time in every dur.
+func Until(t time.Time, dur time.Duration, fn func()) (done <-chan bool) {
+	ch := make(chan bool)
+	go untilRecv(ch, t, dur, fn)
+	return ch
+}
+
+func untilRecv(ch chan bool, t time.Time, dur time.Duration, fn func()) {
+	if t.Sub(time.Now()) > 0 {
+		time.AfterFunc(dur, func() {
+			fn()
+			untilRecv(ch, t, dur, fn)
+		})
+		return
+	}
+	ch <- true
+}
+
+// Runs fn after duration. Similar to time.AfterFunc
+func After(duration time.Duration, fn func()) (done <-chan bool) {
+	ch := make(chan bool)
+	time.AfterFunc(duration, func() {
+		fn()
+		ch <- true
+	})
+	return ch
+}
+
+// Runs fn in every specified duration.
+func Every(dur time.Duration, fn func()) {
+	time.AfterFunc(dur, func() {
+		fn()
+		Every(dur, fn)
+	})
+}
+
+// Runs fn and times out if it runs longer than the provided
+// duration. It will send false to the returning
+// channel if timeout occurs.
+// TODO: cancel if timeout occurs
+func Timeout(duration time.Duration, fn func()) (done <-chan bool) {
+	ch := make(chan bool)
+	go func() {
+		<-time.After(duration)
+		ch <- false
+	}()
+	go func() {
+		fn()
+		ch <- true
+	}()
+	return ch
+}
+
+// Starts a job and returns a channel for cancellation signal.
+// Once a message is sent to the channel, stops the fn.
+func Die(fn func()) (die chan<- bool, done <-chan bool) {
+	ch := make(chan bool)
+	dch := make(chan bool)
+	go func() {
+		<-dch
+		ch <- false
+	}()
+	go func() {
+		fn()
+		ch <- true
+	}()
+	return dch, ch
+}
+
+// Starts to run the given list of fns concurrently.
+func All(fns ...func()) (done <-chan bool) {
+	var wg sync.WaitGroup
+	wg.Add(len(fns))
+
+	ch := make(chan bool)
+	for _, fn := range fns {
+		go func(f func()) {
+			f()
+			wg.Done()
+		}(fn)
+	}
+	go func() {
+		wg.Wait()
+		ch <- true
+	}()
+	return ch
+}
+
+// Starts to run the given list of fns concurrently,
+// at most n fns at a time.
+func AllWithThrottle(throttle int, fns ...func()) (done <-chan bool) {
+	ch := make(chan bool)
+	go func() {
+		for {
+			num := throttle
+			if throttle > len(fns) {
+				num = len(fns)
+			}
+			next := fns[:num]
+			fns = fns[num:]
+			<-All(next...)
+			if len(fns) == 0 {
+				ch <- true
+				break
+			}
+		}
+	}()
+	return ch
+}
+
+// Run the same function with n copies.
+func Replicate(n int, fn func()) (done <-chan bool) {
+	var wg sync.WaitGroup
+	wg.Add(n)
+	ch := make(chan bool)
+	for i := 0; i < n; i++ {
+		go func() {
+			fn()
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		ch <- true
+	}()
+	return ch
+}
---
+++ coop_test.go
@@ -0,0 +1,181 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package coop
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAt_Future(test *testing.T) {
+	start := time.Now()
+	done := At(start.Add(100*time.Millisecond), func() {
+		diff := time.Now().Sub(start)
+		if diff > 105*time.Millisecond {
+			test.Errorf("Expected to run in 100 ms, it did in %v", diff)
+		}
+	})
+	<-done
+}
+
+func TestAt_Past(test *testing.T) {
+	start := time.Now()
+	done := At(start.Add(-100*time.Millisecond), func() {})
+	<-done
+	diff := time.Now().Sub(start)
+	if diff > time.Millisecond {
+		test.Errorf("Expected to return immediately, but it took %v", diff)
+	}
+}
+
+func TestAfter_Future(test *testing.T) {
+	start := time.Now()
+	done := After(100*time.Millisecond, func() {
+		diff := time.Now().Sub(start)
+		if diff > 105*time.Millisecond {
+			test.Errorf("Expected to run in 100 ms, it did in %v", diff)
+		}
+	})
+	<-done
+}
+
+func TestEvery(test *testing.T) {
+	dur := 10 * time.Millisecond
+	count := 0
+	Every(dur, func() {
+		count++
+	})
+	<-time.After(100 * time.Millisecond)
+	count++
+	if count < 9 {
+		test.Errorf("Expected to run in at least 9 times, it did %v times", count)
+	}
+}
+
+func TestUntil_Future(test *testing.T) {
+	count := 0
+	done := Until(time.Now().Add(100*time.Millisecond), 10*time.Millisecond, func() {
+		count++
+	})
+	<-done
+	if count < 9 {
+		test.Errorf("Expected to run for at least for 9 times, but it ran for %v times", count)
+	}
+}
+
+func TestUntil_Past(test *testing.T) {
+	count := 0
+	done := Until(time.Now().Add(-100*time.Millisecond), 10*time.Millisecond, func() {
+		count++
+	})
+	<-done
+	if count != 0 {
+		test.Errorf("Expected to run for at least for 0 times, but it ran for %v times", count)
+	}
+}
+
+func TestTimeout_TimedOut(test *testing.T) {
+	done := Timeout(100*time.Millisecond, func() {
+		time.Sleep(time.Minute)
+	})
+	if <-done {
+		test.Errorf("Expected to get timed out, but it has been completed")
+	}
+}
+
+func TestTimeout_Completed(test *testing.T) {
+	done := Timeout(time.Minute, func() {
+		time.Sleep(100 * time.Millisecond)
+	})
+	if !<-done {
+		test.Errorf("Expected to get completed, but it has been timed out")
+	}
+}
+
+func TestDie_Dead(test *testing.T) {
+	die, done := Die(func() {
+		time.Sleep(time.Minute)
+	})
+	time.AfterFunc(50*time.Millisecond, func() {
+		die <- true
+	})
+	if <-done {
+		test.Errorf("Expected to be dead, but it didn't")
+	}
+}
+
+func TestDie_Completed(test *testing.T) {
+	_, done := Die(func() {
+		time.Sleep(100 * time.Millisecond)
+	})
+	if !<-done {
+		test.Errorf("Expected to be completed, but it didn't")
+	}
+}
+
+func TestAll(test *testing.T) {
+	start := time.Now()
+	var val1, val2, val3 bool
+	done := All(func() {
+		val1 = true
+		time.Sleep(100 * time.Millisecond)
+	}, func() {
+		val2 = true
+		time.Sleep(100 * time.Millisecond)
+	}, func() {
+		val3 = true
+		time.Sleep(100 * time.Millisecond)
+	})
+	<-done
+	diff := time.Now().Sub(start)
+	if diff > 105*time.Millisecond {
+		test.Errorf("All takes too long to complete")
+	}
+	if !(val1 && val2 && val3) {
+		test.Errorf("Expected all to run, but at least one didn't")
+	}
+}
+
+func TestAllWithThrottle(test *testing.T) {
+	start := time.Now()
+	fn := func() {
+		time.Sleep(100 * time.Millisecond)
+	}
+	done := AllWithThrottle(3, fn, fn, fn, fn, fn)
+	<-done
+	diff := time.Now().Sub(start)
+	if diff > 205*time.Millisecond {
+		test.Errorf("All with throttle takes too long to complete")
+	}
+	if diff < 105*time.Millisecond {
+		test.Errorf("All with throttle doesn't take long, throttling may not work")
+	}
+}
+
+func TestReplicate(test *testing.T) {
+	results := make(chan bool, 5)
+	done := Replicate(5, func() {
+		results <- true
+	})
+	<-done
+	close(results)
+	count := 0
+	for _ = range results {
+		count++
+	}
+	if count != 5 {
+		test.Errorf("Expected 5 to run, but %v worked", count)
+	}
+}
